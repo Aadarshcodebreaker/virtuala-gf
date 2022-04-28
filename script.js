122181194c56eea4931af02098cd01dd05f7776e
Aadarshcodebreaker <98023712+Aadarshcodebreaker@users.noreply.github.com>
2022-04-28 08:45:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+ var yourname = prompt("What is your name?");
+ alert(yourname + ' hello im your new gf nezuko nice to meet you');
+ function complement(){
+     alert(yourname + 'you look smart and handsome')
+ }
+ function love(){
+    alert(' I love you' + yourname)
+}
+function lovescore(){
+    alert('GREAT!'  + yourname + 'our lovescore is 100%')
+}
+function kiss(){
+    alert('💋💋💋')
+}
+function order(){
+  var food = prompt('What would you like to eat')
+  alert('nezuko ordered the same' + food)
+}
+function flirt(){
+    alert('dont you get tired, because you are always running in my mind')
+    prompt('flirt with nezuko')
+    alert('😘😘😘')
+}
+function hello(){
+    var hi = prompt('say Hello or Hi')
+    if (hi == hello){
+        alert('hi' + yourname)
+    }
+    else{
+        alert(hi + yourname)
+    }
+    
+}
